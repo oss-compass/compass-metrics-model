94c885eaea22253b613816be1a96097d860342a2
YangWenhao3906 <675451361@qq.com>
2023-08-02 20:02:11 +0800

    split `bug_issue_open_time` metric
    Signed-off-by: YangWenhao3906 <675451361@qq.com>

--- compass_model/productivity/community_service_and_support_metrics_model.py
+++ compass_model/productivity/community_service_and_support_metrics_model.py
@@ -42,27 +42,27 @@ class CommunityServiceAndSupportMetricsModel(BaseMetricsModel):
                     "weight": BUG_ISSUE_OPEN_TIME_WEIGHT_COMMUNITY,
                     "threshold": BUG_ISSUE_OPEN_TIME_THRESHOLD_COMMUNITY
                 },
-                "comment_frequency": {
-                    "weight": COMMENT_FREQUENCY_WEIGHT_COMMUNITY,
-                    "threshold": COMMENT_FREQUENCY_THRESHOLD_COMMUNITY
-                },                
-                "updated_issue_count": {
-                    "weight": UPDATED_ISSUES_WEIGHT_COMMUNITY,
-                    "threshold": UPDATED_ISSUES_THRESHOLD_COMMUNITY
-                },
-                "pr_open_time": {
-                    "weight": PR_OPEN_TIME_WEIGHT_COMMUNITY,
-                    "threshold": PR_OPEN_TIME_THRESHOLD_COMMUNITY
-                },
+                # "comment_frequency": {
+                #     "weight": COMMENT_FREQUENCY_WEIGHT_COMMUNITY,
+                #     "threshold": COMMENT_FREQUENCY_THRESHOLD_COMMUNITY
+                # },                
+                # "updated_issue_count": {
+                #     "weight": UPDATED_ISSUES_WEIGHT_COMMUNITY,
+                #     "threshold": UPDATED_ISSUES_THRESHOLD_COMMUNITY
+                # },
+                # "pr_open_time": {
+                #     "weight": PR_OPEN_TIME_WEIGHT_COMMUNITY,
+                #     "threshold": PR_OPEN_TIME_THRESHOLD_COMMUNITY
+                # },
                 "code_review_count": {
                     "weight": CODE_REVIEW_WEIGHT_COMMUNITY,
                     "threshold": CODE_REVIEW_THRESHOLD_COMMUNITY
                 },
 
-                "closed_prs_count": {
-                    "weight": CLOSED_PRS_WEIGHT_COMMUNITY,
-                    "threshold": CLOSED_PRS_THRESHOLD_COMMUNITY
-                },
+                # "closed_prs_count": {
+                #     "weight": CLOSED_PRS_WEIGHT_COMMUNITY,
+                #     "threshold": CLOSED_PRS_THRESHOLD_COMMUNITY
+                # },
             }
         if level == "community":
             metrics_weights_thresholds = {
@@ -74,27 +74,27 @@ class CommunityServiceAndSupportMetricsModel(BaseMetricsModel):
                     "weight": BUG_ISSUE_OPEN_TIME_WEIGHT_COMMUNITY,
                     "threshold": BUG_ISSUE_OPEN_TIME_MULTIPLE_THRESHOLD_COMMUNITY
                 },
-                "comment_frequency": {
-                    "weight": COMMENT_FREQUENCY_WEIGHT_COMMUNITY,
-                    "threshold": COMMENT_FREQUENCY_MULTIPLE_THRESHOLD_COMMUNITY
-                },                
-                "updated_issue_count": {
-                    "weight": UPDATED_ISSUES_WEIGHT_COMMUNITY,
-                    "threshold": UPDATED_ISSUES_MULTIPLE_THRESHOLD_COMMUNITY
-                },
-                "pr_open_time": {
-                    "weight": PR_OPEN_TIME_WEIGHT_COMMUNITY,
-                    "threshold": PR_OPEN_TIME_MULTIPLE_THRESHOLD_COMMUNITY
-                },
+                # "comment_frequency": {
+                #     "weight": COMMENT_FREQUENCY_WEIGHT_COMMUNITY,
+                #     "threshold": COMMENT_FREQUENCY_MULTIPLE_THRESHOLD_COMMUNITY
+                # },                
+                # "updated_issue_count": {
+                #     "weight": UPDATED_ISSUES_WEIGHT_COMMUNITY,
+                #     "threshold": UPDATED_ISSUES_MULTIPLE_THRESHOLD_COMMUNITY
+                # },
+                # "pr_open_time": {
+                #     "weight": PR_OPEN_TIME_WEIGHT_COMMUNITY,
+                #     "threshold": PR_OPEN_TIME_MULTIPLE_THRESHOLD_COMMUNITY
+                # },
                 "code_review_count": {
                     "weight": CODE_REVIEW_WEIGHT_COMMUNITY,
                     "threshold": CODE_REVIEW_MULTIPLE_THRESHOLD_COMMUNITY
                 },
 
-                "closed_prs_count": {
-                    "weight": CLOSED_PRS_WEIGHT_COMMUNITY,
-                    "threshold": CLOSED_PRS_MULTIPLE_THRESHOLD_COMMUNITY
-                },
+                # "closed_prs_count": {
+                #     "weight": CLOSED_PRS_WEIGHT_COMMUNITY,
+                #     "threshold": CLOSED_PRS_MULTIPLE_THRESHOLD_COMMUNITY
+                # },
             }
         super().__init__(repo_index, git_index, issue_index, pr_index, issue_comments_index, pr_comments_index,
                          contributors_index, release_index, out_index, from_date, end_date, level, community, source,
