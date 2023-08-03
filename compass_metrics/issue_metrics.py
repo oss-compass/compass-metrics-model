f84694da26471f651edb70417842ed0864ab9f08
YangWenhao3906 <yangwh0828@gmail.com>
2023-08-03 14:39:48 +0800

    Complete metrics splitting for community-service-and-support model - (may need to change) I use func `get_time_diff_days` from grimoire_elk
    Signed-off-by: YangWenhao3906 <yangwh0828@gmail.com>

---
+++ compass_metrics/issue_metrics.py
@@ -0,0 +1,103 @@
+from compass_metrics.db_dsl import get_uuid_count_query
+from datetime import timedelta
+from grimoire_elk.enriched.utils import get_time_diff_days
+from grimoirelab_toolkit.datetime import str_to_datetime
+from compass_common.math_utils import get_medium
+
+
+def issue_first_reponse(client, issue_index, date, repos_list):
+    query_issue_first_reponse_avg = get_uuid_count_query(
+        "avg", repos_list, "time_to_first_attention_without_bot", "grimoire_creation_date", size=0, from_date=date-timedelta(days=90), to_date=date)
+    query_issue_first_reponse_avg["query"]["bool"]["must"].append({"match_phrase": {"pull_request": "false"}})
+
+    issue_first_reponse = client.search(index=issue_index, body=query_issue_first_reponse_avg)
+    if issue_first_reponse["hits"]["total"]["value"] == 0:
+        return None, None
+    issue_first_reponse_avg = issue_first_reponse['aggregations']["count_of_uuid"]['value']
+
+    query_issue_first_reponse_mid = get_uuid_count_query(
+        "percentiles", repos_list, "time_to_first_attention_without_bot", "grimoire_creation_date", size=0, from_date=date-timedelta(days=90), to_date=date)
+    query_issue_first_reponse_mid["aggs"]["count_of_uuid"]["percentiles"]["percents"] = [
+        50]
+    query_issue_first_reponse_mid["query"]["bool"]["must"].append({"match_phrase": {"pull_request": "false"}})
+    issue_first_reponse_mid = client.search(index=issue_index, body=query_issue_first_reponse_mid)[
+        'aggregations']["count_of_uuid"]['values']['50.0']
+
+    result = {
+        "issue_first_reponse_avg": issue_first_reponse_avg,
+        "issue_first_reponse_mid": issue_first_reponse_mid
+    }
+    return result
+
+
+def bug_issue_open_time(client, issue_index, date, repos_list):
+    query_issue_opens = get_uuid_count_query("avg", repos_list, "time_to_first_attention_without_bot",
+                                                    "grimoire_creation_date", size=10000, from_date=date-timedelta(days=90), to_date=date)
+    query_issue_opens["query"]["bool"]["must"].append({"match_phrase": {"pull_request": "false" }})
+    bug_query = {
+        "bool": {
+            "should": [{
+                "script": {
+                    "script": "if (doc.containsKey('labels') && doc['labels'].size()>0) { for (int i = 0; i < doc['labels'].length; ++i){ if(doc['labels'][i].toLowerCase().indexOf('bug')!=-1|| doc['labels'][i].toLowerCase().indexOf('缺陷')!=-1){return true;}}}"
+                }
+            },
+                {
+                "script": {
+                    "script": "if (doc.containsKey('issue_type') && doc['issue_type'].size()>0) { for (int i = 0; i < doc['issue_type'].length; ++i){ if(doc['issue_type'][i].toLowerCase().indexOf('bug')!=-1 || doc['issue_type'][i].toLowerCase().indexOf('缺陷')!=-1){return true;}}}"
+                }
+            }],
+            "minimum_should_match": 1
+        }
+    }
+    query_issue_opens["query"]["bool"]["must"].append(bug_query)
+    issue_opens_items = client.search(
+        index=issue_index, body=query_issue_opens)['hits']['hits']
+    if len(issue_opens_items) == 0:
+        return None, None
+    issue_open_time_repo = []
+    for item in issue_opens_items:
+        if 'state' in item['_source']:
+            if item['_source']['closed_at'] and item['_source']['state'] in ['closed', 'rejected'] and str_to_datetime(item['_source']['closed_at']) < date:
+                issue_open_time_repo.append(get_time_diff_days(
+                    item['_source']['created_at'], item['_source']['closed_at']))
+            else:
+                issue_open_time_repo.append(get_time_diff_days(
+                    item['_source']['created_at'], str(date)))
+    issue_open_time_repo_avg = sum(issue_open_time_repo)/len(issue_open_time_repo)
+    issue_open_time_repo_mid = get_medium(issue_open_time_repo)
+    result = {
+        "issue_open_time_repo_avg": issue_open_time_repo_avg,
+        "issue_open_time_repo_mid": issue_open_time_repo_mid
+    }
+    return result
+
+
+def comment_frequency(client, issue_index, date, repo_list):
+    """ Determine the average number of comments per issue created in the last 90 days. """
+    query_issue_comments_count = get_uuid_count_query(
+        "sum", repo_list, "num_of_comments_without_bot", date_field='grimoire_creation_date', size=0,
+        from_date=(date - timedelta(days=90)), to_date=date)
+    query_issue_comments_count["query"]["bool"]["must"].append({"match_phrase": {"pull_request": "false"}})
+    issue = client.search(index=issue_index, body=query_issue_comments_count)
+    try:
+        comment_count = float(issue['aggregations']["count_of_uuid"]['value'] / issue["hits"]["total"]["value"])
+    except ZeroDivisionError:
+        comment_count = None
+    result = {
+        'comment_frequency': float(round(comment_count, 4)) if comment_count is not None else None
+    }
+    return result
+
+
+def updated_issues_count(client, issue_index, date, repo_list):
+    """ Determine the number of issues updated in the last 90 days. """
+    query_issue_updated_since = get_uuid_count_query("cardinality", repo_list, "uuid",
+                                                     date_field='metadata__updated_on', size=0,
+                                                     from_date=(date-timedelta(days=90)), to_date=date)
+    query_issue_updated_since["query"]["bool"]["must"].append({"match_phrase": {"pull_request": "false" }})
+    issues_count = client.search(index=issue_index, body=query_issue_updated_since)[
+        'aggregations']["count_of_uuid"]['value']
+    result = {
+        'updated_issues_count': issues_count
+    }
+    return result
---
+++ compass_metrics/pr_metrics.py
@@ -0,0 +1,68 @@
+from compass_metrics.db_dsl import get_uuid_count_query
+from compass_metrics.db_dsl import get_pr_closed_uuid_count
+from datetime import timedelta
+from grimoire_elk.enriched.utils import get_time_diff_days
+from grimoirelab_toolkit.datetime import str_to_datetime
+from compass_common.math_utils import get_medium
+
+
+def pr_open_time(client, pr_index, date, repos_list):
+    query_pr_opens = get_uuid_count_query("avg", repos_list, "time_to_first_attention_without_bot",
+                                                "grimoire_creation_date", size=10000, from_date=date-timedelta(days=90), to_date=date)
+    query_pr_opens["query"]["bool"]["must"].append({"match_phrase": {"pull_request": "true"}})
+    pr_opens_items = client.search(
+        index=pr_index, body=query_pr_opens)['hits']['hits']
+    if len(pr_opens_items) == 0:
+        return None, None
+    pr_open_time_repo = []
+    for item in pr_opens_items:
+        if 'state' in item['_source']:
+            if item['_source']['state'] == 'merged' and item['_source']['merged_at'] and str_to_datetime(item['_source']['merged_at']) < date:
+                pr_open_time_repo.append(get_time_diff_days(
+                    item['_source']['created_at'], item['_source']['merged_at']))
+            if item['_source']['state'] == 'closed' and str_to_datetime(item['_source']['closed_at'] or item['_source']['updated_at']) < date:
+                pr_open_time_repo.append(get_time_diff_days(
+                    item['_source']['created_at'], item['_source']['closed_at'] or item['_source']['updated_at']))
+            else:
+                pr_open_time_repo.append(get_time_diff_days(
+                    item['_source']['created_at'], str(date)))
+    if len(pr_open_time_repo) == 0:
+        return None, None
+    pr_open_time_repo_avg = float(sum(pr_open_time_repo)/len(pr_open_time_repo))
+    pr_open_time_repo_mid = get_medium(pr_open_time_repo)
+    
+    result = {
+        "pr_open_time_repo_avg": pr_open_time_repo_avg,
+        "pr_open_time_repo_mid": pr_open_time_repo_mid
+    }
+    
+    return result
+
+
+def code_review_count(client, pr_index, date, repo_list):
+    query_pr_comments_count = get_uuid_count_query(
+        "sum", repo_list, "num_review_comments_without_bot", size=0, from_date=(date - timedelta(days=90)),
+        to_date=date)
+    query_pr_comments_count["query"]["bool"]["must"].append({"match_phrase": {"pull_request": "true"}})
+    prs = client.search(index=pr_index, body=query_pr_comments_count)
+    try:
+        comment_count = prs['aggregations']["count_of_uuid"]['value'] / prs["hits"]["total"]["value"]
+    except ZeroDivisionError:
+        comment_count = None
+    result = {
+        'code_review_count': round(comment_count, 4) if comment_count is not None else None
+    }
+    return result
+
+
+def closed_pr_count(client, pr_index, date, repos_list):
+    query_pr_closed = get_pr_closed_uuid_count(
+        "cardinality", repos_list, "uuid", from_date=(date-timedelta(days=90)), to_date=date)
+    pr_closed = client.search(index=pr_index, body=query_pr_closed)[
+        'aggregations']["count_of_uuid"]['value']
+    
+    result = {
+        "pr_closed": pr_closed
+    }
+    
+    return result
