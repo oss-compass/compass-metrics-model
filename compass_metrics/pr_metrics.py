22b6213737006f6a801fa4ff54b8b77485a4e7fa
lishengbao <lishengbao147@gmail.com>
2023-08-18 17:52:13 +0800

    Splitting the pr_issue_linked_ratio, code_merge_ratio and change_request_closure_ratio metrics
    Signed-off-by: lishengbao <lishengbao147@gmail.com>

--- compass_metrics/pr_metrics.py
+++ compass_metrics/pr_metrics.py
@@ -56,6 +56,7 @@ def code_review_count(client, pr_index, date, repo_list):
 
 
 def close_pr_count(client, pr_index, date, repos_list):
+    """ The number of PR accepted and declined in the last 90 days. """
     query_pr_closed = get_pr_closed_uuid_count(
         "cardinality", repos_list, "uuid", from_date=(date-timedelta(days=90)), to_date=date)
     pr_closed = client.search(index=pr_index, body=query_pr_closed)[
@@ -96,12 +97,11 @@ def pr_time_to_first_response(client, pr_index, date, repos_list):
 def change_request_closure_ratio(client, pr_index, date, repos_list):
     """Measures the ratio between the total number of open change requests and the total number
     of closed change requests from the beginning to now."""
-    closure_ratio, closed_count, created_count = change_request_closure(
-        client, pr_index, str_to_datetime("1970-01-01"), date, repos_list)
+    close_pr_count = total_create_close_pr_count(client, pr_index, date, repos_list)
+    pr_count = total_pr_count(client, pr_index, date, repos_list)
+
     result = {
-        "change_request_closure_ratio": closure_ratio,
-        "change_request_closed_count": closed_count,
-        "change_request_created_count": created_count
+        "change_request_closure_ratio": close_pr_count/pr_count if pr_count > 0 else None
     }
     return result
 
@@ -109,39 +109,13 @@ def change_request_closure_ratio(client, pr_index, date, repos_list):
 def change_request_closure_ratio_recently_period(client, pr_index, date, repos_list):
     """Measures the ratio between the total number of open change requests and the total number
     of closed change requests in the last 90 days."""
-    closure_ratio, closed_count, created_count = change_request_closure(
-        client, pr_index, date - timedelta(days=90), date, repos_list)
+    close_pr_count = create_close_pr_count(client, pr_index, date, repos_list)
+    pr_count = pr_count(client, pr_index, date, repos_list)
+
     result = {
-        "change_request_closure_ratio_recently_period": closure_ratio,
-        "change_request_closed_count_recently_period": closed_count,
-        "change_request_created_count_recently_period": created_count
+        "change_request_closure_ratio_recently_period": close_pr_count/pr_count if pr_count > 0 else None
     }
     return result
-
-
-def change_request_closure(client, pr_index, from_date, to_date, repos_list):
-    """Measures the ratio between the total number of open change requests and the total number
-        of closed change requests in the range from_date and to_date"""
-    pr_total_dsl = get_uuid_count_query(
-        "cardinality", repos_list, "uuid", size=0, from_date=from_date, to_date=to_date)
-    pr_total_dsl["query"]["bool"]["must"].append({"match_phrase": {"pull_request": "true"}})
-    pr_total_dsl["aggs"]["count_of_uuid"]["cardinality"]["precision_threshold"] = 100000
-    pr_total_count = client.search(index=pr_index, body=pr_total_dsl)['aggregations']["count_of_uuid"]['value']
-
-    pr_closed_dsl = pr_total_dsl
-    pr_closed_dsl["query"]["bool"]["must"][0]["bool"]["filter"].append({
-                                    "range": {
-                                        "closed_at": {
-                                            "gte": from_date.strftime("%Y-%m-%d"),
-                                            "lt": to_date.strftime("%Y-%m-%d")
-                                        }
-                                    }
-                                })
-    pr_closed_count = client.search(index=pr_index, body=pr_closed_dsl)['aggregations']["count_of_uuid"]['value']
-    try:
-        return round(pr_closed_count / pr_total_count, 4), pr_closed_count, pr_total_count
-    except ZeroDivisionError:
-        return None, 0, 0
 
 
 def code_review_ratio(client, pr_index, date, repos_list):
@@ -155,10 +129,11 @@ def code_review_ratio(client, pr_index, date, repos_list):
 
 
 def pr_count(client, pr_index, date, repos_list):
-    """ The number of PR created in the last 90 days. """
+    """ The number of PR created from the beginning to now """
     query_pr_count = get_uuid_count_query(
         "cardinality", repos_list, "uuid", size=0, from_date=(date-timedelta(days=90)), to_date=date)
     query_pr_count["query"]["bool"]["must"].append({"match_phrase": {"pull_request": "true"}})
+    query_pr_count["aggs"]["count_of_uuid"]["cardinality"]["precision_threshold"] = 100000
     pr_count = client.search(index=pr_index, body=query_pr_count)['aggregations']["count_of_uuid"]['value']
     result = {
         "pr_count": pr_count
@@ -179,11 +154,92 @@ def pr_count_with_review(client, pr_index, date, repos_list):
 
 
 def code_merge_ratio(client, pr_index, date, repos_list):
+    """ Determine the percentage of PR Mergers and PR authors who are not the same person in the last 90 days of commits. """
+    merge_count_with_non_author = code_merge_count_with_non_author(client, pr_index, date, repos_list)
+    merge_count = code_merge_count(client, pr_index, date, repos_list)
+    
+    result = {
+        "code_merge_ratio": merge_count_with_non_author/merge_count if merge_count > 0 else None
+    }
+    return result
+
+
+def pr_issue_linked_ratio(client, pr_index, pr_comments_index, date, repos_list):
+    """ Determine the percentage of new pull request link issues in the last 90 days. """
+    code_pr_count = pr_count(client, pr_index, date, repos_list)
+    code_pr_issue_linked_count = pr_issue_linked_count(client, pr_index, pr_comments_index, date, repos_list)
+    result = {
+        "pr_issue_linked_ratio": code_pr_issue_linked_count/code_pr_count if code_pr_count > 0 else None
+    }
+    return result
+
+
+def total_create_close_pr_count(client, pr_index, date, repos_list):
+    """The number of pr created from the beginning to now and it has been accepted and declined"""
+    from_date = str_to_datetime("1970-01-01")
+    pr_total_dsl = get_uuid_count_query(
+        "cardinality", repos_list, "uuid", size=0, from_date=from_date, to_date=date)
+    pr_total_dsl["query"]["bool"]["must"].append({"match_phrase": {"pull_request": "true"}})
+    pr_total_dsl["aggs"]["count_of_uuid"]["cardinality"]["precision_threshold"] = 100000
+
+    pr_closed_dsl = pr_total_dsl
+    pr_closed_dsl["query"]["bool"]["must"][0]["bool"]["filter"].append({
+                                    "range": {
+                                        "closed_at": {
+                                            "gte": from_date.strftime("%Y-%m-%d"),
+                                            "lt": date.strftime("%Y-%m-%d")
+                                        }
+                                    }
+                                })
+    pr_closed_count = client.search(index=pr_index, body=pr_closed_dsl)['aggregations']["count_of_uuid"]['value']
+    result = {
+        "total_create_close_pr_count": pr_closed_count
+    }
+    return result
+
+
+def total_pr_count(client, pr_index, date, repos_list):
+    """ The number of PR created in the last 90 days. """
+    query_pr_count = get_uuid_count_query(
+        "cardinality", repos_list, "uuid", size=0, from_date=str_to_datetime("1970-01-01"), to_date=date)
+    query_pr_count["query"]["bool"]["must"].append({"match_phrase": {"pull_request": "true"}})
+    query_pr_count["aggs"]["count_of_uuid"]["cardinality"]["precision_threshold"] = 100000
+    pr_count = client.search(index=pr_index, body=query_pr_count)['aggregations']["count_of_uuid"]['value']
+    result = {
+        "total_pr_count": pr_count
+    }
+    return result
+
+
+def create_close_pr_count(client, pr_index, date, repos_list):
+    """The number of pr created in the last 90 days and it has been accepted and declined"""
+    from_date = date - timedelta(days=90)
+    pr_total_dsl = get_uuid_count_query(
+        "cardinality", repos_list, "uuid", size=0, from_date=from_date, to_date=date)
+    pr_total_dsl["query"]["bool"]["must"].append({"match_phrase": {"pull_request": "true"}})
+    pr_total_dsl["aggs"]["count_of_uuid"]["cardinality"]["precision_threshold"] = 100000
+
+    pr_closed_dsl = pr_total_dsl
+    pr_closed_dsl["query"]["bool"]["must"][0]["bool"]["filter"].append({
+                                    "range": {
+                                        "closed_at": {
+                                            "gte": from_date.strftime("%Y-%m-%d"),
+                                            "lt": date.strftime("%Y-%m-%d")
+                                        }
+                                    }
+                                })
+    pr_closed_count = client.search(index=pr_index, body=pr_closed_dsl)['aggregations']["count_of_uuid"]['value']
+    result = {
+        "create_close_pr_count": pr_closed_count
+    }
+    return result
+
+
+def code_merge_count_with_non_author(client, pr_index, date, repos_list):
+    """ Determine the Numbers of PR Mergers and PR authors who are not the same person in the last 90 days of commits. """
     query_pr_body = get_uuid_count_query("cardinality", repos_list, "uuid", "grimoire_creation_date", size=0, from_date=(date-timedelta(days=90)), to_date=date)
     query_pr_body["query"]["bool"]["must"].append({"match_phrase": {"pull_request": "true"}})
     query_pr_body["query"]["bool"]["must"].append({"match_phrase": {"merged": "true"}})
-    pr_merged_count = client.search(index=pr_index, body=query_pr_body)[
-        'aggregations']["count_of_uuid"]['value']
     query_pr_body["query"]["bool"]["must"].append({
                         "script": {
                             "script": "if(doc['merged_by_data_name'].size() > 0 && doc['author_name'].size() > 0 && doc['merged_by_data_name'].value !=  doc['author_name'].value){return true}"
@@ -193,29 +249,33 @@ def code_merge_ratio(client, pr_index, date, repos_list):
         'aggregations']["count_of_uuid"]['value']
     
     result = {
-        "code_merge_ratio": prs/pr_merged_count if pr_merged_count > 0 else None,
-        "code_merge": prs,
-        "code_merge_total": pr_merged_count
+        "code_merge_count_with_non_author": prs
     }
     return result
 
 
-def pr_issue_linked_ratio(client, pr_index, pr_comments_index, date, repos_list):
+def code_merge_count(client, pr_index, date, repos_list):
+    """ Determine the Numbers of PR merge in the last 90 days of commits. """
+    query_pr_body = get_uuid_count_query("cardinality", repos_list, "uuid", "grimoire_creation_date", size=0, from_date=(date-timedelta(days=90)), to_date=date)
+    query_pr_body["query"]["bool"]["must"].append({"match_phrase": {"pull_request": "true"}})
+    query_pr_body["query"]["bool"]["must"].append({"match_phrase": {"merged": "true"}})
+    pr_merged_count = client.search(index=pr_index, body=query_pr_body)[
+        'aggregations']["count_of_uuid"]['value']
+    result = {
+        "code_merge_count": pr_merged_count
+    }
+    return result
+
+
+def pr_issue_linked_count(client, pr_index, pr_comments_index, date, repos_list):
+    """ Determine the Numbers of new pull request link issues in the last 90 days. """
     pr_linked_issue = 0
     for repo in repos_list:
         query_pr_linked_issue = get_pr_linked_issue_count(
             repo, from_date=date-timedelta(days=90), to_date=date)
         pr_linked_issue += client.search(index=(pr_index, pr_comments_index), body=query_pr_linked_issue)[
             'aggregations']["count_of_uuid"]['value']
-    query_pr_count = get_uuid_count_query(
-        "cardinality", repos_list, "uuid", size=0, from_date=(date-timedelta(days=90)), to_date=date)
-    query_pr_count["query"]["bool"]["must"].append({"match_phrase": {"pull_request": "true"}})
-    pr_count = client.search(index=pr_index,
-                             body=query_pr_count)[
-        'aggregations']["count_of_uuid"]['value']
     result = {
-        "pr_issue_linked_ratio": pr_linked_issue/pr_count if pr_count > 0 else None,
-        "total_pr": pr_count,
-        "pr_issue_linked": pr_linked_issue,
+        "pr_issue_linked_count": pr_linked_issue,
     }
     return result
--- compass_model/base_metrics_model.py
+++ compass_model/base_metrics_model.py
@@ -54,6 +54,12 @@ from compass_metrics.pr_metrics import (code_review_count,
                                         pr_time_to_first_response,
                                         change_request_closure_ratio,
                                         change_request_closure_ratio_recently_period,
+                                        total_create_close_pr_count,
+                                        total_pr_count,
+                                        create_close_pr_count,
+                                        code_merge_count_with_non_author,
+                                        code_merge_count,
+                                        pr_issue_linked_count,
                                         )
 from typing import Dict, Any
 
@@ -339,7 +345,13 @@ class BaseMetricsModel:
             "pr_issue_linked_ratio": lambda: pr_issue_linked_ratio(self.client, self.pr_index, self.pr_comments_index, date, repo_list),
             "pr_time_to_first_response": lambda: pr_time_to_first_response(self.client, self.pr_index, date, repo_list),
             "change_request_closure_ratio": lambda: change_request_closure_ratio(self.client, self.pr_index, date, repo_list),
-            "change_request_closure_ratio_recently_period": lambda: change_request_closure_ratio_recently_period(self.client, self.pr_index, date, repo_list),
+            "total_create_close_pr_count": lambda: total_create_close_pr_count(self.client, self.pr_index, date, repo_list),
+            "total_pr_count": lambda: total_pr_count(self.client, self.pr_index, date, repo_list),
+            "create_close_pr_count": lambda: create_close_pr_count(self.client, self.pr_index, date, repo_list),
+            "code_merge_count_with_non_author": lambda: code_merge_count_with_non_author(self.client, self.pr_index, date, repo_list),
+            "code_merge_count": lambda: code_merge_count(self.client, self.pr_index, date, repo_list),
+            "pr_issue_linked_count": lambda: pr_issue_linked_count(self.client, self.pr_index, self.pr_comments_index, date, repo_list),
+
             # repo
             "recent_releases_count": lambda: recent_releases_count(self.client, self.release_index, date, repo_list),
             # contributor
