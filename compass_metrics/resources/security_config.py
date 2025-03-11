f6f0f261568c7f788b71194f2752695020245bf9
huliwei <liwei.oss@isrc.iscas.ac.cn>
2025-03-11 15:50:44 +0800

    将配置信息存储到配置文件上
    Signed-off-by: huliwei <liwei.oss@isrc.iscas.ac.cn>

---
+++ compass_metrics/resources/security_config.py
@@ -0,0 +1,7 @@
+TPC_SERVICE_API_USERNAME = "temporary_user"
+TPC_SERVICE_API_PASSWORD = "default_password"
+TPC_SERVICE_API_ENDPOINT = "http://119.12.172.232:8082"
+# 线上回调地址
+TPC_SERVICE_SERVICE_CALLBACK_URL = "https://oss-compass.org/api/tpc_software_callback"
+# 测试环境回调地址
+TPC_SERVICE_SERVICE_CALLBACK_URL_TEST = "http://159.138.38.244/api/tpc_software_callback"
